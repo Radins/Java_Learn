055f467103ff4dcc6f54ca1c311194e6ac897e5b
Felipe Radins <f_radins@hotmail.com>
2021-06-22 20:42:21 -0300

    Learing some more things

--- A_Simple_Java_Program.java
+++ A_Simple_Java_Program.java
@@ -1,5 +1,12 @@
+import javax.xml.transform.sax.SAXResult;
+
 public class A_Simple_Java_Program {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        int n1 =2;
+        int n2 = 5;
+        double result = n2/n1;
+
+        System.out.print(result);
     }
 }
